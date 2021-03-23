6cfaefe1198cb845a32e37d7c0d081a8c91cb238
AliAlkasasbeh <alikasasbeh60@yahoo.com>
2021-03-23 23:44:44 +0200

    lab3

--- js/app.js
+++ js/app.js
@@ -1,67 +1,114 @@
+
+
 'use strict';
 
 // alert('hello') ;
 let arrOfObject=[];
-function Product(image_url,title,description,keyword,horns)
+let keyWord = [];
+let alltitle   = [];
+let inx = 0;
+
+function Product(image_url,title,description,key,horns)
 {
 this.image_url =image_url;
 this.title     =title;
 this.description =description;
-this.keyword   =keyword;
+this.key = key;
 this.horns     =horns;
+this.id = inx;
+alltitle.push(this.title);
 arrOfObject.push(this);
+
 }
+
+let proSection
 Product.prototype.render =function(){
-    let proSection = $('#photo-template').clone();
+     proSection = $('#photo-template').clone();
+     $('main').append(proSection);
     // proSection.children("img").attr('src',this.image_url);
     proSection.find('h2').text(this.title);
     proSection.find('img').attr('src',this.image_url);
     proSection.find('p').text(this.description);
     proSection.removeAttr('id');
-    $('main').append(proSection);
-
-    // let prosel = $('select').clone();
-    // prosel.find('option').val(this.title);
-    // $('header').append(prosel);
-
+    proSection.attr('id', this.id);
+    inx++;
 }
-// let t ='Reem';
-// $('select').append(`<option value="ali" selected="selected">${t}</option>`);
 
-// function test(){
-    
-// for(let i=0;i<10;i++){
-//     console.log(i);
-// }}
-// test();
 Product.prototype.renderSelect =function(){
-$('select').append(`<option value="ali" selected="selected">${this.keyword}</option>`);
+    if (!keyWord.includes(this.key)){
+        keyWord.push(this.key)
+        let newOption = $('<option></option>');
+        $('select').append(newOption);
+        newOption.text(this.key);
+    }}
+console.log()
+function renderSelect () {
+    $('select').on('change', function(){
+       let  $selected = $('select').val();
+    //    if ($selected !='default'){
+    //        $('div').hide();
+    //        arrOfObject.forEach(element=>{
+    //            if(element.key === $selected){
+    //                console.log(element.key);
+    //                $('#'+element.id).show();
+    //            }
+    //        });
+           
+    //    }else{
+    //        $('div').show();
+    //    }
 
+        for (let i = 0; i<inx; i++){
+            if (arrOfObject[i].key == $selected) {
+                $('#'+i).show();
+             }
+            else{
+                if($('select').val() == 'default'){
+                    $('#'+i).show();
+                }else{
+                $('#'+i).hide();
+            }}
+        }
+    });
 }
-
-
 function getJsonData(){
 const ajaxSet ={
     method :'get',
     datatype :'json'
 }
-// console.log("test if i use the ajax");
+
+
 $.ajax('data/page-1.json',ajaxSet).then(data=>{
-// console.log('test got the data');
-// console.log(data);
 let proObj
 let checkArr=[];
 data.forEach(element => {
    proObj =new Product(element.image_url,element.title,element.description,element.keyword,element.horns) ;
+   
+
    proObj.render(); 
-//    console.log(element);
-//    checkArr.push()
    proObj.renderSelect();
 });
-//    proObj.renderSelect();
 
 });
 }
+
 // getJsonData();
 $('document').ready(getJsonData);
+renderSelect();
 
+// $(document).ready(function(){
+//     $("#btn-1").click(function(){
+//         $("section").toggle();
+//         $("#photo-template").hide();
+//     });
+//   });
+
+$(document).ready(function(){
+    $("#sort-AZ").click(function(){
+        // alert('hello');
+        // console.log(arrOfObject);
+       arrOfObject.sort((a, z) => a.title.charCodeAt() - z.title.charCodeAt());
+        // console.log(sort);
+        
+    });
+  });
